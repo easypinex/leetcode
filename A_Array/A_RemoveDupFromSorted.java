759f9a4c09d56e22adb324713edcdd6ff462d9b0
Andy <easypinex@gmail.com>
2021-01-22 14:47:51 +0800

    新增英文題目說明

--- A_Array/A_RemoveDupFromSorted.java
+++ A_Array/A_RemoveDupFromSorted.java
@@ -2,9 +2,14 @@ package A_Array;
 
 import java.util.Arrays;
 
-/**
- * 1-Remove
- */
+/*
+Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+Clarification:
+Confused why the returned value is an integer but your answer is an array?
+Note that the input array is passed in by reference, which means a modification to the input array will be known to the caller as well.
+... https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/727
+*/
 public class A_RemoveDupFromSorted {
     public static void main(String[] args) {
         int[] sortedArr = { 1, 1, 2, 2, 3 };
