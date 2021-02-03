16b9d174040ba1618989db852838227d216d386f
Andy <easypinex@gmail.com>
2021-02-03 14:53:46 +0800

    倒轉字串

---
+++ B_String/A_ReverseString.java
@@ -0,0 +1,35 @@
+package B_String;
+
+import java.util.Arrays;
+
+/*
+Write a function that reverses a string. The input string is given as an array of characters char[].
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+You may assume all the characters consist of printable ascii characters. 
+
+Example 1:
+
+Input: ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+Example 2:
+
+Input: ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+*/
+public class A_ReverseString {
+    public static void main(String[] args) {
+        char[] s = new char[] { 'H', 'a', 'n', 'n', 'a', 'h' };
+        new A_ReverseString().reverseString(s);
+        System.out.println(Arrays.toString(s));
+    }
+
+    public void reverseString(char[] s) {
+        char temp;
+        for (int i = 0; i < s.length / 2; i++) {
+            temp = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - 1 - i] = temp;
+        }
+    }
+}
